e77c712dd8c4b1d7c191b26ce85882c73bc8b9d4
Spencer Gunning <swcgunning@gmail.com>
2017-08-31 17:22:53 -0400

    [0.0.1-b3] Changed private data to example data

--- ubudget.py
+++ ubudget.py
@@ -6,7 +6,7 @@ class c: r, g, w, x = '\033[1;31m', '\033[1;32m', '\033[1;37m', '\033[0m'
 j = json.load(open('data.json'))
 xt, yt, zt = j["p"][:]
 bn, bd, bt, ba = j['t'][0]['n'], j['t'][0]['d'], j['t'][0]['t'], j['t'][0]['a']
-n, d, s, x = len(ba), [], [],0
+n, d, s, x = len(ba), [], [], 0
 ss, xs, ys, zs = [0] * 4
 
 t = ['', c.w + 'All (100%)' + c.x, c.w + 'Saving (' + str(xt) + '%)' + c.x,
